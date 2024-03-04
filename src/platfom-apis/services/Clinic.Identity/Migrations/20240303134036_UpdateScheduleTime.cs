5241b6de8915512497cc881f9e6da5c51585dd44
DerekNguyen <datnht2602@gmail.com>
2024-03-04 07:11:46 +0700

    update

---
+++ src/platfom-apis/services/Clinic.Identity/Migrations/20240303134036_UpdateScheduleTime.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clinic.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateScheduleTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScheduleTimes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Time = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleTimes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScheduleTimes_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleTimes_UserId",
+                table: "ScheduleTimes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleTimes");
+        }
+    }
+}
