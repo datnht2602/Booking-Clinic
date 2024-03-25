4756ca093d99e286978539f5a113d392de1e73b3
Bthao <thaontbde160404@fpt.edu.vn>
2024-03-25 19:52:40 +0700

    update

--- src/platfom-apis/services/Clinic.Booking/Services/BookingService.cs
+++ src/platfom-apis/services/Clinic.Booking/Services/BookingService.cs
@@ -34,33 +34,35 @@ namespace Clinic.Booking.Services
         }
         public async Task<ResponseDto> AddBookingAsync(BookingDetailsViewModel booking)
         {
-            ResponseDto result = new();
-            ArgumentValidation.ThrowIfNull(booking);
+            ResponseDto result = new();//create new object to store the final result of appointment adding process 
+            ArgumentValidation.ThrowIfNull(booking);//ensure the booking argument is not null, if null -> throw an exception
            var getExistingBooking = await this.GetBookingAsync($" e.UserId = '{booking.UserId}' and e.OrderStatus= '{OrderStatus.Cart}' and e.id= '{booking.Id}'").ConfigureAwait(false);
+                //call GetBookingAsync to get a list of appointments include UserID, OrderStatus and AppointmentID
            BookingDetailsViewModel? existingBooking = getExistingBooking.FirstOrDefault();
-           if(existingBooking != null){
+                //if appointment exists according to the given condition, it will take the first appointment from the list and assign it to the existingBooking variable.
+            if (existingBooking != null){ //if appointment not null, method will update booking ID and Etag from existingBooking
             booking.Id = existingBooking.Id;
             booking.Etag = existingBooking.Etag;
             if(booking.OrderStatus == OrderStatus.Submitted.ToString()){
                 
             }
             
-            await this.UpdateBookingAsync(booking).ConfigureAwait(false);
-            result.Result = booking;
+            await this.UpdateBookingAsync(booking).ConfigureAwait(false);//calls the UpdateBookingAsync method to update the appointment and then returns the results.
+                result.Result = booking;
             return result;
            }else{
             booking.OrderStatus = OrderStatus.Cart.ToString();
             booking.OrderTotal = booking.Products.Sum(x => x.Price);
-            var bookingModel = autoMapper.Map<Data.Models.Booking>(booking);
-            using var bookingRequest = new StringContent(JsonSerializer.Serialize(bookingModel),Encoding.UTF8,ContentType);
+            var bookingModel = autoMapper.Map<Data.Models.Booking>(booking);//maps the booking to a Booking object in database form and creates a StringContent from this object.
+                using var bookingRequest = new StringContent(JsonSerializer.Serialize(bookingModel),Encoding.UTF8,ContentType);
             var bookingResponse = await this.httpClient.PostAsync(new Uri($"{applicationSettings.Value.DataStoreEndpoint}getbooking"),bookingRequest).ConfigureAwait(false);
-
-            if(!bookingResponse.IsSuccessStatusCode){
+                //request to the backend API to create a new appointment
+           if (!bookingResponse.IsSuccessStatusCode){
                 await this.ThrowServiceToServiceErrors(bookingResponse).ConfigureAwait(false);
             }
             var createdBookingDAO = await bookingResponse.Content.ReadFromJsonAsync<Clinic.Data.Models.Booking>().ConfigureAwait(false);
-
-            var createdBooking = autoMapper.Map<BookingDetailsViewModel>(createdBookingDAO);
+                //reads the generated appointment information from the response.
+            var createdBooking = autoMapper.Map<BookingDetailsViewModel>(createdBookingDAO);//new appointment result is mapped to a BookingDetailsViewModel object and the result is returned
                 result.Result = booking;
                 return result;
            }
